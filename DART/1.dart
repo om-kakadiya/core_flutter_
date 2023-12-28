9532dc079138ba304d3cbdeee706aab296477372
om-kakadiya <143987215+om-kakadiya@users.noreply.github.com>
2023-12-28 16:43:39 +0530

    Add files via upload

---
+++ DART/1.dart
@@ -0,0 +1,17 @@
+void main() {
+  Set mySet = {10,20,30,40,"i am a dart"};
+
+  // mySet.add(20);
+  // mySet.addAll({500, 1000});
+
+  // mySet.remove(20);
+  // mySet.removeAll({500, 1000});
+
+  // mySet.removeWhere((mySet) => mySet % 2 == 0);
+
+  mySet.first;
+ mySet.last(mySet);
+  mySet.length;
+
+  print(mySet);
+}
---
+++ DART/4.dart
@@ -0,0 +1,9 @@
+void main() {
+  Set S1 = {1, 2, 3, 4, 5};
+
+  Set S2 = {2, 3, 4, 5, 6, 7};
+
+  S1.union(S2);
+
+  print(S1);
+}
